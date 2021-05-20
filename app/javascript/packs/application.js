5f39f1e0819154596132d8cd6507cb5b9a4a62b1
ToroChu <whileonrails@gmail.com>
2021-05-20 23:44:09 +0800

    add vue instance & column component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,68 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-import "../stylesheets";
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context("../images", true);
+const imagePath = (name) => images(name, true);
+
+import "../stylesheets";
+
+// Vue
+import TurbolinksAdapter from "vue-turbolinks";
+import Vue from "vue/dist/vue.esm";
+import BoardColumn from "./components/kanban/column";
+import axios from "axios-on-rails";
+
+// import Rails from "@rails/ujs";
+// 裡面打ajax跟資料庫取得資料。
+// 而在做這一步前，記得在開頭要引入ujs
+// import Rails from '@rails/ujs'
+// 這可以讓我們可以在不更動原有的controller的情況下，就能用Rails的慣例處理route與action。
+// beforeMount(){
+//   // 打API
+//   Rails.ajax({
+//     url: `/kanbans/${this.kanban_id}/columns.json`,
+//     type: 'GET',
+//     dataType: 'json',
+//     success: result => {
+//       // 把ajax回傳結果放進columns陣列
+//       this.columns = result;
+//     },
+//     error: error => {
+//       console.log(error);
+//     }
+//   });
+// }
+
+Vue.use(TurbolinksAdapter);
+
+document.addEventListener("turbolinks:load", () => {
+  let el = document.querySelector("#column");
+
+  if (el) {
+    new Vue({
+      el,
+      components: { BoardColumn },
+      data() {
+        return {
+          kanban_id: el.dataset.kanbanid,
+          columns: [],
+        };
+      },
+      beforeMount() {
+        axios
+          .get(`/kanbans/${this.kanban_id}/columns.json`)
+          .then((res) => {
+            console.log("res: ", res);
+            this.columns = res.data;
+          })
+          .catch((error) => {
+            console.log(error.response);
+          });
+      },
+    });
+  }
+});
---
+++ app/javascript/packs/components/kanban/column.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="column">
+    <div class="column-name">{{ column.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Column",
+  props: {
+    column: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.column {
+  @apply bg-gray-100 mx-2 w-64 rounded;
+
+  .column-name {
+    @apply px-3 py-2 font-thin;
+  }
+}
+</style>

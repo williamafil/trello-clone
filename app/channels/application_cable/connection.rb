c4652bdfd4d82fd002522740aa23989f8b370ac1
ToroChu <whileonrails@gmail.com>
2021-06-29 14:29:58 +0800

    add notification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,14 +9,9 @@ module ApplicationCable
     private
 
     def find_verified_user
-      puts "= = = = request.env['warden'].user = = = ="
-      puts request.env['warden'].user
       if current_user = request.env['warden'].user
-        puts '= = = = current_user: = = = ='
-        puts current_user.name
         current_user
       else
-        puts '= = = = 使用者未登入，拒絕連線 = = = ='
         reject_unauthorized_connection
       end
     end

b831d54d13daa770c0bb41ede2a8359ef53e50ba
ToroChu <whileonrails@gmail.com>
2021-07-03 15:11:50 +0800

    fix tailwind path

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ require("channels");
 // const images = require.context("../images", true);
 // const imagePath = (name) => images(name, true);
 
-import "../stylesheets";
+import "../stylesheets/index.js";
 
 // Vue
 import TurbolinksAdapter from "vue-turbolinks";

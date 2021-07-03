61d476c2e47c9c223e4b806fa81969a28684fbb0
ToroChu <whileonrails@gmail.com>
2021-07-03 16:03:51 +0800

    fix action cable url

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module RailsVueKanban
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    ActionCable.server.config.disable_request_forgery_protection = true
   end
 end

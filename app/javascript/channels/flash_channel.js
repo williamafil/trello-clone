f57ab61c1b610a1ce7b508d84ee15017cea00dd0
ToroChu <whileonrails@gmail.com>
2021-07-05 13:36:50 +0800

    edit web socket console.log message

--- app/javascript/channels/flash_channel.js
+++ app/javascript/channels/flash_channel.js
@@ -1,27 +1,30 @@
 import consumer from "./consumer";
-console.log("flash channel 連線中...");
+console.log("Connecting to Flash Channel...");
 
-consumer.subscriptions.create({
-  channel: "FlashChannel",
-}, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("flash channel 連線成功!");
+consumer.subscriptions.create(
+  {
+    channel: "FlashChannel",
   },
+  {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log("Flash Channel is connected!");
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-    console.log("flash channel 已斷線");
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+      console.log("Flash Channel is disconnected!");
+    },
+
+    received(flashData) {
+      // Called when there's incoming data on the websocket for this channel
+      // console.log("這裡是 flash channel ");
+      // console.log("flashData: ", flashData);
+
+      if (flashData.commit) {
+        // console.log("data.commit: ", flashData.commit);
+        window.$store.commit(flashData.commit, flashData.payload);
+      }
+    },
   },
-
-  received(flashData) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log("這裡是 flash channel ");
-    console.log("flashData: ", flashData);
-
-    if (flashData.commit) {
-      console.log("data.commit: ", flashData.commit);
-      window.$store.commit(flashData.commit, flashData.payload);
-    }
-  },
-});
+);
